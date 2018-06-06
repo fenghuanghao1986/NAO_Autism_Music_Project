cf504636c301e6074a3f20b71aa5ce5282fa0f77
FingerStyleFinn <hfeng7@du.edu>
2018-06-06 16:33:27 -0600

    delete non-related work

--- Homework1/q3.py
+++
@@ -1,9 +0,0 @@
-def count(start, step, stop):
-    while(start <= stop):
-        print(start)
-        start += step
-
-print("Test number set 1:")
-count(0,2,20)
-print("Test number set 2:")
-count(3,4,17)
--- Homework1/q7.py
+++
@@ -1,8 +0,0 @@
-def Favorite_Animal(name):
-    checkList = {'Sophia': 'Cat', 'Victoria': 'Dog', 'Madison': 'Elephant', 'Bill': 'Lion'}
-    names = list(checkList.keys())
-    if name in names:
-        print("Hi ", + str(name) + ", your favorite animal is ", checkList[name])
-    else:
-        print("ERROR! Name cannot be found in the database! Please do it again!")
-

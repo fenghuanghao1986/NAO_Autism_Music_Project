5d0060f71b12301f72b2960bf63efb131bc44b74
FingerStyleFinn <hfeng7@du.edu>
2018-07-18 21:59:56 -0600

    try to used image
    must test see two things:
    1. if this code can show image captured from NAO camera
    2. if hough circle can be successfully applied to this image

--- NAO_images_pyqt_test.py
+++ NAO_images_pyqt_test.py
@@ -112,7 +112,11 @@ class ImageWidget(QWidget):
 
 
 if __name__ == '__main__':
-    IP = "nao.local"  # Replace here with your NaoQi's IP address.
+    
+    import cv2
+    import numpy as np
+    
+    IP = "192.168.0.1"  # Replace here with your NaoQi's IP address.
     PORT = 9559
     CameraID = 0
 
@@ -128,4 +132,24 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWidget = ImageWidget(IP, PORT, CameraID)
     myWidget.show()
+
+    img = cv2.imread(myWidget._image, 0)
+    # img = cv2.imread('opencv_logo.png',0)
+    img = cv2.medianBlur(img,5)
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                                param1=50,param2=30,minRadius=0,maxRadius=0)
+    
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    
+    cv2.imshow('detected circles',cimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
     sys.exit(app.exec_())

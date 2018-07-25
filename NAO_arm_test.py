7d1c0f48c834d4e034bca2a09044ba05d5f3dd54
FingerStyleFinn <hfeng7@du.edu>
2018-07-25 11:22:29 -0600

    this should be a good start
    test this code

--- NAO_arm_test.py
+++ NAO_arm_test.py
@@ -4,16 +4,16 @@ Created on Tue Jul 24 16:10:58 2018
 
 @author: fengh
 """
-"""
+
 import argparse
 import motion
-import time
+import almath
 from naoqi import ALProxy
 
-def main(robotIP, PORT, chainName):
-    ''' Example of a whole body Left or Right Arm position control
-        Warning: Needs a PoseInit before executing
-                 Whole body balancer must be inactivated at the end of the script
+
+def main(robotIP, PORT=9559):
+    ''' Example showing a path of two positions
+    Warning: Needs a PoseInit before executing
     '''
 
     motionProxy  = ALProxy("ALMotion", robotIP, PORT)
@@ -23,91 +23,29 @@ def main(robotIP, PORT, chainName):
     motionProxy.wakeUp()
 
     # Send robot to Stand Init
-    #postureProxy.goToPosture("StandInit", 0.5)
+    postureProxy.goToPosture("StandInit", 0.5)
 
-    frame     = motion.FRAME_ROBOT
-    useSensor = False
+    effector   = "LArm"
+    frame      = motion.FRAME_TORSO
+    axisMask   = almath.AXIS_MASK_VEL # just control position
+    useSensorValues = False
 
-    effectorInit = motionProxy.getPosition(chainName, frame, useSensor)
+    path = []
+    currentTf = motionProxy.getTransform(effector, frame, useSensorValues)
+    targetTf  = almath.Transform(currentTf)
+    targetTf.r1_c4 += 0.03 # x
+    targetTf.r2_c4 += 0.03 # y
 
-    # Active LArm tracking
-    isEnabled = True
-    motionProxy.wbEnableEffectorControl(chainName, isEnabled)
+    path.append(list(targetTf.toVector()))
+    path.append(currentTf)
 
-    # Example showing how to set position target for LArm
-    # The 3 coordinates are absolute LArm position in FRAME_ROBOT
-    # Position in meter in x, y and z axis.
+    # Go to the target and back again
+    times      = [2.0, 4.0] # seconds
 
-    # X Axis LArm Position feasible movement = [ +0.00, +0.12] meter
-    # Y Axis LArm Position feasible movement = [ -0.05, +0.10] meter
-    # Y Axis RArm Position feasible movement = [ -0.10, +0.05] meter
-    # Z Axis LArm Position feasible movement = [ -0.10, +0.10] meter
-
-    coef = 1.0
-    if chainName == "LArm":
-        coef = +1.0
-    elif chainName == "RArm":
-        coef = -1.0
-
-    targetCoordinateList = [
-    [ +0.12, +0.00*coef, +0.00], # target 0
-    [ +0.12, +0.00*coef, -0.10], # target 1
-    [ +0.12, +0.05*coef, -0.10], # target 1
-    [ +0.12, +0.05*coef, +0.10], # target 2
-    [ +0.12, -0.10*coef, +0.10], # target 3
-    [ +0.12, -0.10*coef, -0.10], # target 4
-    [ +0.12, +0.00*coef, -0.10], # target 5
-    [ +0.12, +0.00*coef, +0.00], # target 6
-    [ +0.00, +0.00*coef, +0.00], # target 7
-    ]
-
-
-    # wbSetEffectorControl is a non blocking function
-    # time.sleep allow head go to his target
-    # The recommended minimum period between two successives set commands is
-    # 0.2 s.
-    for targetCoordinate in targetCoordinateList:
-        targetCoordinate = [targetCoordinate[i] + effectorInit[i] for i in range(3)]
-        motionProxy.wbSetEffectorControl(chainName, targetCoordinate)
-        time.sleep(4.0)
-
-    # Deactivate Head tracking
-    isEnabled    = False
-    motionProxy.wbEnableEffectorControl(chainName, isEnabled)
+    motionProxy.transformInterpolations(effector, frame, path, axisMask, times)
 
     # Go to rest position
     motionProxy.rest()
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="127.0.0.1",
-                        help="Robot ip address")
-    parser.add_argument("--port", type=int, default=9559,
-                        help="Robot port number")
-    parser.add_argument("--chain", type=str, default="LArm",
-                        choices=["LArm", "RArm"], help="Chain name")
-
-    args = parser.parse_args()
-    main(args.ip, args.port, args.chain)
-"""
-import time
-import argparse
-from naoqi import ALProxy
-
-def main(robotIP, PORT=9559):
-    motionProxy = ALProxy("ALMotion", robotIP, PORT)
-
-    motionProxy.setStiffnesses("Head", 1.0)
-
-    # Example showing how to set angles, using a fraction of max speed
-    names  = ["HeadYaw", "HeadPitch"]
-    angles  = [0.2, -0.5]
-    fractionMaxSpeed  = 0.2
-    motionProxy.setAngles(names, angles, fractionMaxSpeed)
-
-    time.sleep(3.0)
-    motionProxy.setStiffnesses("Head", 0.0)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

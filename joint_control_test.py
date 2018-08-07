0c69e54f77dc7a7ed724d01a5c6d82c70ce04143
FingerStyleFinn <hfeng7@du.edu>
2018-08-07 14:19:02 -0600

    it can hit now
    speeed and angle need adjust, then can perform hit bar.

--- joint_control_test.py
+++ joint_control_test.py
@@ -10,30 +10,31 @@ import argparse
 from naoqi import ALProxy
 
 def main(robotIP, PORT = 9559):
-    motionProxy = ALProxy("ALMotion", robotIP, PORT)
-
-    motionProxy.setStiffnesses("LArm", 1.0)
-
-    # Example showing a slow, relative move of "HeadYaw".
-    # Calling this multiple times will move the head further.
-    names = "LWristYaw"
-    hit = 0.3
-    release = -0.3
-    fractionMaxSpeed = 1
-    motionProxy.changeAngles(names, hit, fractionMaxSpeed)
+    while(True):
+        motionProxy = ALProxy("ALMotion", robotIP, PORT)
     
-    time.sleep(0.2)
+        motionProxy.setStiffnesses("RArm", 1.0)
     
-    motionProxy.changeAngles(names, release, fractionMaxSpeed)
-
-    time.sleep(2.0)
-
-    motionProxy.setStiffnesses("LArm", 0.0)
+        # Example showing a slow, relative move of "HeadYaw".
+        # Calling this multiple times will move the head further.
+        names = "RWristYaw"
+        hit = -0.3
+        release = 0.3
+        fractionMaxSpeed = 1
+        motionProxy.changeAngles(names, hit, fractionMaxSpeed)
+        
+        time.sleep(1)
+        
+        motionProxy.changeAngles(names, release, fractionMaxSpeed)
+        
+        time.sleep(1)
+    
+        # motionProxy.setStiffnesses("RArm", 0.0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="127.0.0.1",
+    parser.add_argument("--ip", type=str, default="169.254.254.250",
                         help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")

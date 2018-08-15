867871f2430ccd6de7ea568ce82ad5298a30de50
FingerStyleFinn <hfeng7@du.edu>
2018-08-15 17:01:05 -0600

    create loop
    add a loop to hit 10 notes on 2 bars

--- hard_coding_play_xylophone.py
+++ hard_coding_play_xylophone.py
@@ -47,27 +47,26 @@ def initRobotPosition(motionProxy, postureProxy):
     time.sleep(1.0)
     
 def playHardMode(motionProxy, postureProxy):
-       
-        # Example showing a slow, relative move of "HeadYaw".
-        # Calling this multiple times will move the head further.
-        hit = -0.6
-        release = 0.3
-        fractionMaxSpeed = 1
+    
+    hit = -0.6
+    fractionMaxSpeed = 1
+    
+    for i in range(0,6):  
         
         motionProxy.changeAngles("RWristYaw", hit, fractionMaxSpeed)
         time.sleep(0.2)
+        
         motionProxy.setAngles("RWristYaw", -0.6, 1.0)
         
         time.sleep(0.8)
         #motionProxy.changeAngles("RWristYaw", release, 1)        
-        #time.sleep(0.5)
         
         motionProxy.changeAngles("LWristYaw", -hit, fractionMaxSpeed)        
         time.sleep(0.2)
         
         motionProxy.setAngles("LWristYaw", 0.6, 1.0)
         #motionProxy.changeAngles("LWristYaw", -release, 1)        
-        #time.sleep(0.5)
+        time.sleep(0.8)
     
 def main(robotIP, PORT=9559):
     

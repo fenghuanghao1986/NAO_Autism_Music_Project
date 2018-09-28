532e620faae1968f0aaad545342c29c08891168e
FingerStyleFinn <hfeng7@du.edu>
2018-09-28 13:58:38 -0600

    change code
    this code will only use rgb code
    no hsv involved

--- Vision_Detection_Part/rgb_test_2.py
+++ Vision_Detection_Part/rgb_test_2.py
@@ -4,42 +4,35 @@ Created on Wed Sep 26 13:47:17 2018
 
 @author: CV_LAB_Howard
 """
-
-import cv2
+# import the necessary packages
 import numpy as np
+import argparse
+import cv2
 
-## Read
-img = cv2.imread("xylophone_1280_960_no_beater.jpg")
+# load the image
+image = cv2.imread('xylophone_640_480')
 
-## convert to hsv
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# define the list of boundaries
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
 
-lower_green = np.array([45, 70, 70])
-upper_green = np.array([75, 255, 255])
-
-lower_brown = np.array([0, 128, 128])
-upper_brown = np.array([75, 255, 255])
-
-lower_yello = np.array([20, 150, 150])
-upper_yello = np.array([75, 255, 255])
-
-## mask of green (36,0,0) ~ (70, 255,255)
-mask2 = cv2.inRange(hsv, lower_green, upper_green)
-
-## mask o yellow (15,0,0) ~ (36, 255, 255)
-mask3 = cv2.inRange(hsv, lower_yello, upper_yello)
-
-mask1 = cv2.inRange(hsv, lower_brown, upper_brown)
-
-## final mask and masked
-#mask = cv2.bitwise_or(mask1, mask2, mask3)
-mask = cv2.bitwise_or(mask1, mask2, mask3)
-
-cv2.imshow('image', mask)
-
-target = cv2.bitwise_and(img,img, mask=mask)
-
-
-
-cv2.imwrite("target.png", target)
-
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+ 
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv2.inRange(image, lower, upper)
+	output = cv2.bitwise_and(image, image, mask = mask)
+ 
+	# show the images
+	cv2.imshow("images", np.hstack([image, output]))
+	cv2.waitKey(0)
+    
+    

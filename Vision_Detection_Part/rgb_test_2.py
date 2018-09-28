15102066793c45a6631223ba85258bdc79286385
FingerStyleFinn <hfeng7@du.edu>
2018-09-28 13:39:22 -0600

    testing colors
    seems like, the mask have to be in certain orders

--- Vision_Detection_Part/rgb_test_2.py
+++ Vision_Detection_Part/rgb_test_2.py
@@ -9,26 +9,37 @@ import cv2
 import numpy as np
 
 ## Read
-img = cv2.imread("xylophone_320_240_no_beater.jpg")
+img = cv2.imread("xylophone_1280_960_no_beater.jpg")
 
 ## convert to hsv
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-lower_green = np.array([36, 0, 0])
-upper_green = np.array([70, 255, 255])
+lower_green = np.array([45, 70, 70])
+upper_green = np.array([75, 255, 255])
 
-lower_brown = np.array([15, 0, 0])
-upper_brown = np.array([36, 255, 255])
+lower_brown = np.array([0, 128, 128])
+upper_brown = np.array([75, 255, 255])
 
+lower_yello = np.array([20, 150, 150])
+upper_yello = np.array([75, 255, 255])
 
 ## mask of green (36,0,0) ~ (70, 255,255)
-mask1 = cv2.inRange(hsv, lower_green, upper_green)
+mask2 = cv2.inRange(hsv, lower_green, upper_green)
 
 ## mask o yellow (15,0,0) ~ (36, 255, 255)
-mask2 = cv2.inRange(hsv, lower_brown, upper_brown)
+mask3 = cv2.inRange(hsv, lower_yello, upper_yello)
+
+mask1 = cv2.inRange(hsv, lower_brown, upper_brown)
 
 ## final mask and masked
-mask = cv2.bitwise_or(mask1, mask2)
+#mask = cv2.bitwise_or(mask1, mask2, mask3)
+mask = cv2.bitwise_or(mask1, mask2, mask3)
+
+cv2.imshow('image', mask)
+
 target = cv2.bitwise_and(img,img, mask=mask)
 
+
+
 cv2.imwrite("target.png", target)
+

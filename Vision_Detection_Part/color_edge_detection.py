00022218edee9e3f2a93a17f08f77c19cc38a373
FingerStyleFinn <hfeng7@du.edu>
2018-10-03 14:30:14 -0600

    combined color detection and edge detection
    it has noise, so need to do pre-process first
    still cannot find better filter.
    need to work on that.

--- Vision_Detection_Part/color_edge_detection.py
+++ Vision_Detection_Part/color_edge_detection.py
@@ -9,18 +9,7 @@ Created on Wed Oct  3 10:41:57 2018
 
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
-img = cv2.imread('ball5.jpg',0)
-   
-edges = cv2.Canny(img,100,200)
-
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
-plt.show()
+# from matplotlib import pyplot as plt
 
 # load the image
 image = cv2.imread('xylophone_640_480.jpg')
@@ -28,25 +17,49 @@ image = cv2.imread('xylophone_640_480.jpg')
 # define the list of boundaries
 boundaries = [
     ([0, 0, 200], [100, 100, 255]),     # red
-    ([60, 70, 120], [100, 95, 160]),    # brown 
+    ([40, 50, 120], [100, 90, 160]),    # brown
     ([30, 160, 240], [70, 255, 255]),   # yellow
     ([0, 130, 0], [120, 255, 150]),     # green 
     ([180, 100, 180], [255, 180, 255]), # pink
     ([200, 100, 0], [255, 255, 155]),   # blue
     ([192, 192, 192], [240, 240, 224])  # gray 
 ]
-
+n = 0
 # loop over the boundaries
 for (lower, upper) in boundaries:
-	# create NumPy arrays from the boundaries
-	lower = np.array(lower, dtype = "uint8")
-	upper = np.array(upper, dtype = "uint8")
+    # create NumPy arrays from the boundaries
+    n = n + 1
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
  
-	# find the colors within the specified boundaries and apply
-	# the mask
-	mask = cv2.inRange(image, lower, upper)
-	output = cv2.bitwise_and(image, image, mask = mask)
- 
-	# show the images
-	cv2.imshow("images", np.hstack([image, output]))
-	cv2.waitKey(0)
+	 # find the colors within the specified boundaries and apply
+	 # the mask
+    mask = cv2.inRange(image, lower, upper)
+    
+    output = cv2.bitwise_and(image, image, mask = mask)
+    blurred = cv2.GaussianBlur(output, (5, 5), 0)
+    
+    # cv2.imshow("image", output)
+    # show the images
+    # cv2.imshow("images", np.hstack([image, output]))
+    cv2.imwrite(str(n) + ".jpg", blurred)
+    
+    if n == 1:
+        continue
+    
+    img = cv2.imread(str(n-1) + ".jpg")
+    
+    add = blurred + img
+    cv2.imwrite(str(n) + ".jpg", add)
+
+img = cv2.imread(str(n) + ".jpg", 1)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+# cv2.imshow("image", blurred)
+result = cv2.Canny(blurred, 100, 225)  
+# cv2.imshow("image", result)
+cv2.imshow("images", np.hstack([gray, result]))
+compare = np.hstack([gray, result])
+cv2.imwrite("edge.jpg", result)
+cv2.imwrite("compare.jpg", compare)

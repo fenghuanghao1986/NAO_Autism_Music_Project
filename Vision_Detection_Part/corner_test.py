37e9ba8115929675ebbad3fb41e5bf5ae7b3878f
FingerStyleFinn <hfeng7@du.edu>
2018-10-17 16:01:25 -0600

    asdfdsfa

--- Vision_Detection_Part/corner_test.py
+++ Vision_Detection_Part/corner_test.py
@@ -7,15 +7,18 @@ This is a temporary script file.
 import cv2
 import numpy as np
 
-img = cv2.imread(r'D:\LabWork\ThesisProject\noteDetection\Vision_Detection_Part\1.jpg', 1)
+img = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\2.jpg', 1)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+#blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
-result = cv2.Canny(blurred, 100, 225)
+result = cv2.Canny(gray, 100, 225)
 result = np.float32(result) 
-corner = cv2.cornerHarris(result,3,5,0.1) 
+corner = cv2.cornerHarris(result,3,3,0.08) 
 corner = cv2.dilate(corner, None)
 img[corner>0.01*corner.max()]=[0,0,255]
+cv2.imwrite("corner_img.jpg", img)
 cv2.imshow("image", img)
+
+

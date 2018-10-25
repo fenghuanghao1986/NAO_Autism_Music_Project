e7ac1c1b3479584c699773fe5176c7b53ae8f2d9
FingerStyleFinn <hfeng7@du.edu>
2018-10-25 15:52:29 -0600

    creating function to scan image

--- Vision_Detection_Part/real_lighting_condition_pics/test1.py
+++ Vision_Detection_Part/real_lighting_condition_pics/test1.py
@@ -7,9 +7,16 @@ Created on Fri Oct 19 13:26:07 2018
 import cv2
 import numpy as np
 
-image1 = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\640b.jpg')
-image2 = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\cornerb.jpg')
+image = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\6.jpg')
 
-minus = image1 - image2
+center_blue = [[160, 120, 70], [210, 180, 130]]
 
-cv2.imshow("image", minus)
+for (lower, upper) in center_blue:
+    # create NumPy arrays from the boundaries
+    # n = n + 1
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
+ 
+	 # find the colors within the specified boundaries and apply
+	 # the mask
+    mask = cv2.inRange(image, lower, upper)

634784854e514b7a070b58d81e76d49ceb6393b2
FingerStyleFinn <hfeng7@du.edu>
2018-10-26 12:06:38 -0600

    draw rectangle

--- Vision_Detection_Part/real_lighting_condition_pics/test1.py
+++ Vision_Detection_Part/real_lighting_condition_pics/test1.py
@@ -7,16 +7,32 @@ Created on Fri Oct 19 13:26:07 2018
 import cv2
 import numpy as np
 
-image = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\6.jpg')
+image = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\640b.jpg')
 
-center_blue = [[160, 120, 70], [210, 180, 130]]
+#center_blue = [[160, 120, 70], [210, 180, 130]]
 
-for (lower, upper) in center_blue:
-    # create NumPy arrays from the boundaries
-    # n = n + 1
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
- 
-	 # find the colors within the specified boundaries and apply
-	 # the mask
-    mask = cv2.inRange(image, lower, upper)
+def findBlue(img):
+    
+    result = []    
+    print(img.shape)              
+    height = img.shape[0]        
+    width = img.shape[1]
+    channels = img.shape[2]
+    print("height:%s,width:%s,channels:%s" % (height,width,channels))
+    print(img.size)              
+    for row in range(height):    
+        for col in range(width): 
+            if img[row][col][0] >= 160 and img[row][col][0]<= 210:
+                if img[row][col][1] >= 120 and img[row][col][1]<= 180:
+                    if img[row][col][2] >= 70 and img[row][col][2]<= 130:
+                        result.append((col, row))
+    
+    leftup = result[0]
+    rightbottom = result[-1]
+    cv2.rectangle(img, leftup, rightbottom, (0, 255, 0))
+                   
+    cv2.imshow("image",img) 
+
+if __name__ == "__main__":
+    findBlue(image)
+    

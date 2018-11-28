1fe678ed24e22af32ebcada468aeed83ac1b8377
FingerStyleFinn <hfeng7@du.edu>
2018-11-28 12:27:55 -0700

    change param

--- Vision_Detection_Part/hough_circle.py
+++ Vision_Detection_Part/hough_circle.py
@@ -8,12 +8,14 @@ Created on Wed Jun 27 11:42:22 2018
 import cv2
 import numpy as np
 
-img = cv2.imread('opencv_logo.png',0)
+#image = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\320b.jpg')
+
+img = cv2.imread(r'D:\Howard_Feng\noteDetection\Vision_Detection_Part\real_lighting_condition_pics\320b.jpg',0)
 img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=0,maxRadius=0)
+                            param1=200,param2=30,minRadius=0,maxRadius=0)
 
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:

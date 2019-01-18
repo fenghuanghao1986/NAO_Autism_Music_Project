4f5c73fbd480bf951353978b36f02846cddd2f58
FingerStyleFinn <hfeng7@du.edu>
2019-01-17 21:08:23 -0700

    changing IP

--- Motion_Part/getTransfrom_test.py
+++ Motion_Part/getTransfrom_test.py
@@ -12,7 +12,7 @@ from naoqi import ALProxy
 def main(robotIP, PORT=9559):
     motionProxy = ALProxy("ALMotion", robotIP, PORT)
     
-    motionProxy.rest()
+#    motionProxy.rest()
     # Example showing how to get the end of the right arm as a transform
     # represented in torso space. The result is a 4 by 4 matrix composed
     # of a 3*3 rotation matrix and a column vector of positions.
@@ -26,7 +26,7 @@ def main(robotIP, PORT=9559):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="127.0.0.1",
+    parser.add_argument("--ip", type=str, default="192.168.0.2",
                         help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")

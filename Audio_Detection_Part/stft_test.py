77bfbbc516bd9e022bad09d80c4b6f57a21ecf03
FingerStyleFinn <hfeng7@du.edu>
2019-01-29 21:16:01 -0700

    test stft
    figure out how to use this stft code
    and plot the frequency regarding time
    
    also add music for testing

--- Audio_Detection_Part/stft_test.py
+++ Audio_Detection_Part/stft_test.py
@@ -1,117 +1,133 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 26 18:50:01 2019
-
-@author: CV_LAB_Howard
-"""
-
-# a test code for stft
-
-from scipy import signal
-import matplotlib.pyplot as plt
-import soundfile as sf
-from bandpass_fft_series_getPeaks import butter_bandpass_filter 
 import numpy as np
-import scipy.io.wavfile as wavfile
-from scipy.interpolate import interp1d
-from scipy.signal import argrelextrema
-#def stft(x, Nwin, Nfft=None):
-#    """
-#    Short-time Fourier transform: convert a 1D vector to a 2D array
-#    The short-time Fourier transform (STFT) breaks a long vector into disjoint
-#    chunks (no overlap) and runs an FFT (Fast Fourier Transform) on each chunk.
-#    The resulting 2D array can 
-#    Parameters
-#    ----------
-#    x : array_like
-#        Input signal (expected to be real)
-#    Nwin : int
-#        Length of each window (chunk of the signal). Should be ≪ `len(x)`.
-#    Nfft : int, optional
-#        Zero-pad each chunk to this length before FFT. Should be ≥ `Nwin`,
-#        (usually with small prime factors, for fastest FFT). Default: `Nwin`.
-#    Returns
-#    -------
-#    out : complex ndarray
-#        `len(x) // Nwin` by `Nfft` complex array representing the STFT of `x`.
-#    
-#    See also
-#    --------
-#    istft : inverse function (convert a STFT array back to a data vector)
-#    stftbins : time and frequency bins corresponding to `out`
-#    """
-#    Nfft = Nfft or Nwin
-#    Nwindows = x.size // Nwin
-#    # reshape into array `Nwin` wide, and as tall as possible. This is
-#    # optimized for C-order (row-major) layouts.
-#    arr = np.reshape(x[:Nwindows * Nwin], (-1, Nwin))
-#    stft = np.fft.rfft(arr, Nfft)
-#    return stft
-##
-file = r'D:\Howard_Feng\noteDetection\Audio_Detection_Part\new_xylo\ace.wav'
-##
-waveData, fs = sf.read(file)
-##
-#lowcut = 1000.0
-#highcut = 3000.0
-##
-#y = butter_bandpass_filter(waveData, lowcut, highcut, fs, order=3)
-##
-#
-#zxx = stft(y[:,0], 10)
-#
-#t = len(waveData)/fs
-#
-#
-#
-##
-#
-##fs = 10e3
-##N = 1e5
-##amp = 2 * np.sqrt(2)
-###noise_power = 0.01 * fs / 2
-##time = np.arange(N) / float(fs)
-##mod = 500*np.cos(2*np.pi*0.25*time)
-##carrier = np.sin(2*np.pi*3e3*time + mod)
-##
-##f, t, Zxx = signal.stft(carrier, fs, nperseg=1000)
-## =============================================================================
-#plt.plot(zxx)
-## =============================================================================
-#plt.show()
-##plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=amp)
-##plt.title('STFT Magnitude')
-##plt.ylabel('Frequency [Hz]')
-##plt.xlabel('Time [sec]')
-##plt.show()
+import numpy.fft as fft
+from scipy.io import wavfile as wav
 
-rate, data = wavfile.read(waveData)
-time = np.arange(len(data[:,0]))*1.0/rate
+def stft(x, Nwin, Nfft=None):
+    """
+    Short-time Fourier transform: convert a 1D vector to a 2D array
+    The short-time Fourier transform (STFT) breaks a long vector into disjoint
+    chunks (no overlap) and runs an FFT (Fast Fourier Transform) on each chunk.
+    The resulting 2D array can 
+    Parameters
+    ----------
+    x : array_like
+        Input signal (expected to be real)
+    Nwin : int
+        Length of each window (chunk of the signal). Should be ≪ `len(x)`.
+    Nfft : int, optional
+        Zero-pad each chunk to this length before FFT. Should be ≥ `Nwin`,
+        (usually with small prime factors, for fastest FFT). Default: `Nwin`.
+    Returns
+    -------
+    out : complex ndarray
+        `len(x) // Nwin` by `Nfft` complex array representing the STFT of `x`.
+    
+    See also
+    --------
+    istft : inverse function (convert a STFT array back to a data vector)
+    stftbins : time and frequency bins corresponding to `out`
+    """
+    Nfft = Nfft or Nwin
+    Nwindows = x.size // Nwin
+    # reshape into array `Nwin` wide, and as tall as possible. This is
+    # optimized for C-order (row-major) layouts.
+    arr = np.reshape(x[:Nwindows * Nwin], (-1, Nwin))
+    stft = fft.rfft(arr, Nfft)
+    return stft
 
-#plt.plot(time,data[:,0])
-#plt.show()
 
-nfft = 1024*6
-pxx, freq, bins, plot = plt.specgram(data[:,0],NFFT=nfft)
+def stftbins(x, Nwin, Nfft=None, d=1.0):
+    """
+    Time and frequency bins corresponding to short-time Fourier transform.
+    Call this with the same arguments as `stft`, plus one extra argument: `d`
+    sample spacing, to get the time and frequency axes that the output of
+    `stft` correspond to.
+    Parameters
+    ----------
+    x : array_like
+        same as `stft`
+    Nwin : int
+        same as `stft`
+    Nfft : int, optional
+        same as `stft`
+    d : float, optional
+        Sample spacing of `x` (or 1 / sample frequency), units of seconds.
+        Default: 1.0.
+    Returns
+    -------
+    t : ndarray
+        Array of length `len(x) // Nwin`, in units of seconds, corresponding to
+        the first dimension (height) of the output of `stft`.
+    f : ndarray
+        Array of length `Nfft`, in units of Hertz, corresponding to the second
+        dimension (width) of the output of `stft`.
+    """
+    Nfft = Nfft or Nwin
+    Nwindows = x.size // Nwin
+    t = np.arange(Nwindows) * (Nwin * d)
+    f = fft.rfftfreq(Nfft, d)
+    return t, f
 
-plt.show()
 
-a = np.mean(pxx,axis=0)
-aa = np.arange(len(a))
-a = a/np.max(a)*np.max(data[:,0])
-aa = aa/np.max(aa) * time[-1]
+def istft(stftArr, Nwin):
+    """
+    Inverse short-time Fourier transform (ISTFT)
+    Given an array representing the output of `stft`, convert it back to the
+    original samples.
+    Parameters
+    ----------
+    stftArr : ndarray
+        Output of `stft` (or something the same size)
+    Nwin : int
+        Same input as `stft`: length of each chunk that the STFT was calculated
+        over.
+    
+    Returns
+    -------
+    y : ndarray
+        Data samples corresponding to STFT data.
+    
+    See also:
+    stft : the forward transform
+    """
+    arr = fft.irfft(stftArr)[:, :Nwin]
+    return np.reshape(arr, -1)
 
-f = interp1d(aa,a)
-newSmooth = f(time)
 
-indMax = argrelextrema(newSmooth, np.greater)[0]
-indMin = argrelextrema(newSmooth, np.less)[0]
+if __name__ == '__main__':
+    
+    file = r'D:\Howard_Feng\noteDetection\Audio_Detection_Part\promise.wav'
+    sampleRate, data = wav.read(file)
+    N = len(data)
+    Nwin = 4096
+    x = data[:, 0]
+    
+    # Generate a chirp: start frequency at 5 Hz and going down at 2 Hz/s
+    time = np.arange(N) / sampleRate  # seconds
+#    x = np.cos(2 * np.pi * time * (5 - 2 * 0.5 * time))
 
-lastValue = np.where(newSmooth==newSmooth[-1])[0]
-indMin = np.hstack((indMin,lastValue))
+    # Test with Nfft bigger than Nwin
+    Nfft = Nwin * 2
+    s = stft(x, Nwin)
+    y = istft(s, Nwin)
 
-plt.plot(time,data[:,0])
-plt.plot(aa,a)
-plt.plot(time[indMax],newSmooth[indMax])
-plt.plot(time[indMin],newSmooth[indMin])
-plt.show()
+    # Make sure the stft and istft are inverses. Caveat: `x` and `y` won't be
+    # the same length if `N/Nwin` isn't integral!
+#    maxerr = np.max(np.abs(x - y))
+#    assert (maxerr < np.spacing(1) * 10)
+
+    # Test `stftbins`
+    t, f = stftbins(x, Nwin, d=1.0 / sampleRate)
+    assert (len(t) == s.shape[0])
+    assert (len(f) == s.shape[1])
+ 
+    try:
+        import pylab as plt
+        plt.imshow(np.abs(s), aspect="auto", extent=[f[0], f[-1], t[-1], t[0]])
+        plt.xlabel('frequency (Hertz)')
+        plt.ylabel('time (seconds (start of chunk))')
+        plt.title('STFT with chirp example')
+        plt.grid()
+        plt.show()
+    except ModuleNotFoundError:
+        pass

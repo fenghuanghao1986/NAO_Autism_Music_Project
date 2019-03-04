36eb3fe1060d8d93079621ced5187fed1b589a5e
FingerStyleFinn <hfeng7@du.edu>
2019-03-03 21:46:54 -0700

    getposition is totally not working
    it gives me around 1inch of position off
    totally not worth it.
    may have to try setAngle this is like the last option for me now.
    getTransform can work, but need luck.

--- Motion_Part/getTransfrom_test.py
+++ Motion_Part/getTransfrom_test.py
@@ -11,36 +11,50 @@ import time
 from naoqi import ALProxy
 
 def main(robotIP, PORT=9559):
+    
     motionProxy = ALProxy("ALMotion", robotIP, PORT)
+    motionProxy.rest()
     motionProxy.setStiffnesses("LArm", 0)
     motionProxy.setStiffnesses("RArm", 1)
-#    motionProxy.rest()
+    
+    time.sleep(1)
+        
     # Example showing how to get the end of the right arm as a transform
     # represented in torso space. The result is a 4 by 4 matrix composed
     # of a 3*3 rotation matrix and a column vector of positions.
+    
     name  = 'RArm'
     print(name)
     frame  = motion.FRAME_TORSO
-    useSensorValues  = True
-    transResult = motionProxy.getTransform(name, frame, useSensorValues)
-    print(transResult)
-    
-    name = "RWristYaw"
-    print(name)
+    useSensorValues  = False
+    transResult = motionProxy.getPosition(name, frame, useSensorValues)
     angResult = motionProxy.getAngles(name, useSensorValues)
     print(angResult)
+    print(transResult)
     
+#    name = 'RWristYaw'
+#    print(name)
+#    angResult = motionProxy.getAngles(name, useSensorValues)
+#    print(angResult)
+#    posResult = motionProxy.getPosition(name, frame, useSensorValues)
+#    print(posResult)
+    
+    
+    chainName        = 'RArm'
+    frame            = motion.FRAME_TORSO
 
-#    chainName        = "LArm"
-#    frame            = motion.FRAME_TORSO
-#
-#    transform       = [0.8993398547172546, -0.3961007595062256, 0.1851811408996582, 0.1086173802614212, 0.43249425292015076, 0.7435457110404968, -0.5099887251853943, 0.23731710016727448, 0.0643162727355957, 0.5387429594993591, 0.8400115966796875, 0.03261344134807587, 0.0, 0.0, 0.0, 1.0]
-#    
-#    fractionMaxSpeed = 0.5
-#    axisMask         = 63
-#
-#    motionProxy.setTransforms(chainName, frame, transform, fractionMaxSpeed, axisMask)
-#    time.sleep(1)
+    transform       = [0.13809768855571747, -0.1352280229330063, -0.05866047367453575, 1.5619783401489258, 0.6344230771064758, -0.2303130328655243]
+    
+    fractionMaxSpeed = 0.5
+    axisMask         = 63
+
+    motionProxy.setPosition(chainName, frame, transform, fractionMaxSpeed, axisMask)
+    time.sleep(1)
+    
+    transResult = motionProxy.getPosition(name, frame, useSensorValues)
+    angResult = motionProxy.getAngles(name, useSensorValues)
+    print(angResult)
+    print(transResult)
 ##    
 #    hit = -0.25
 #    fractionMaxSpeed = 0.2

8b7b3b53e9c20049071d375017364d6c6f611382
FingerStyleFinn <hfeng7@du.edu>
2019-03-03 22:40:21 -0700

    setangle tested
    need to take a look at the data and get better sense

--- Motion_Part/getangles_test.py
+++ Motion_Part/getangles_test.py
@@ -2,18 +2,32 @@
 
 import argparse
 from naoqi import ALProxy
+import time
 
 def main(robotIP, PORT=9559):
     motionProxy = ALProxy("ALMotion", robotIP, PORT)
+    
+    motionProxy.rest()
+    motionProxy.setStiffnesses("LArm", 0)
+    motionProxy.setStiffnesses("RArm", 0)
+    
+    time.sleep(5)
+    motionProxy.setStiffnesses("RArm", 1)
 
     # Example that finds the difference between the command and sensed angles.
-    names         = "Body"
+    names         = "RArm"
     useSensors    = False
-    commandAngles = motionProxy.getAngles(names, useSensors)
+    commandAngles = [0.6550600528717041, -0.3145120143890381, 1.405102014541626, 0.6811380386352539, -0.7470998764038086, 0.23559999465942383]
     print "Command angles:"
     print str(commandAngles)
     print ""
 
+    angles  = commandAngles
+    fractionMaxSpeed  = 0.2
+    motionProxy.setAngles(names, angles, fractionMaxSpeed)
+
+    time.sleep(5.0)
+    
     useSensors  = True
     sensorAngles = motionProxy.getAngles(names, useSensors)
     print "Sensor angles:"
@@ -31,10 +45,10 @@ def main(robotIP, PORT=9559):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-#    parser.add_argument("--ip", type=str, default="192.168.0.2",
-#                        help="Robot ip address")
-    parser.add_argument("--ip", type=str, default="127.0.0.1",
+    parser.add_argument("--ip", type=str, default="192.168.0.2",
                         help="Robot ip address")
+#    parser.add_argument("--ip", type=str, default="127.0.0.1",
+#                        help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")
 

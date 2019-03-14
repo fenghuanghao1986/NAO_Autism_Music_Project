0c0f17134d3cc201dc6c768bcff5f0a29b9c2c63
FingerStyleFinn <hfeng7@du.edu>
2019-03-14 12:09:01 -0600

    Update LED_test.py
    eye color changes
    however need to have try catch
    and also need to have color change back as well
    otherwise it always have that color on the eyes will make uncomfortable to kid I believe

--- Session1_6_7/LED_test.py
+++ Session1_6_7/LED_test.py
@@ -11,16 +11,17 @@ Created on Thu Mar 14 10:40:02 2019
 
 import sys
 from naoqi import ALProxy
-robotIP = '127.0.0.1'
+#robotIP = '127.0.0.1'
+robotIP = '192.168.0.2'
 PORT = 9559
 # Example showing how to switch on a group
 ledProxy = ALProxy("ALLeds", robotIP, PORT)
-name = 'LeftFaceLeds'
-colorName = 'red'
+#name = 'LeftFaceLeds'
+#colorName = 'red'
 #name = 'RightFaceLeds'
 #colorName = 'green'
-#name = 'ChestLeds'
-#colorName = 'blue'
+name = 'ChestLeds'
+colorName = 'blue'
 duration = 2.0
 ledProxy.fadeRGB(name, colorName, duration)
 sys.exit(0)

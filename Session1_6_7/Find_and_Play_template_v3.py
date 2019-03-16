7d79bff6a68f03d5b0a0ae93f96f94767ebe1f29
FingerStyleFinn <hfeng7@du.edu>
2019-03-16 16:17:27 -0600

    Update Find_and_Play_template_v3.py
    adding for control loop
    finished tasks
    need to add task 8 for free play
    test the rest first
    may think of how to have led play with the music

--- Session1_6_7/Find_and_Play_template_v3.py
+++ Session1_6_7/Find_and_Play_template_v3.py
@@ -12,8 +12,8 @@ import almath
 import time
 import argparse
 import motion
-import sys
 from naoqi import ALProxy
+
 # =============================================================================
 # these notes is for temp use, later have to save them in one file and import
 # =============================================================================
@@ -43,7 +43,7 @@ notes[10] = [1.3207321166992188, 0.8283181190490723, -1.679771900177002, -0.8620
 notes[11] = [1.3529460430145264, 0.931096076965332, -1.679771900177002, -0.7500841617584229, 0.7638900279998779, 0.225600004196167]
 
 # =============================================================================
-# 
+# The following function initializes NAO's starting position or resting position
 # =============================================================================
 def userInitPosture(motionProxy, postureProxy):
 # =============================================================================
@@ -79,28 +79,42 @@ def userInitPosture(motionProxy, postureProxy):
     
     motionProxy.setStiffnesses("LLeg", 0.2)
     motionProxy.setStiffnesses("RLeg", 0.2)
-    
-#    motionProxy.setAngles("LArm", [0.9234261512756348, 0.3451080322265625, -1.4542741775512695, -0.8283181190490723, 1.2839161157608032, 0.23400002717971802]
-#, 0.3)
-#    motionProxy.setAngles("RArm", 0.0, 1.0)
     time.sleep(1.0)
     
-#    handName  = 'LHand'
-#    motionProxy.openHand(handName)
-#    time.sleep(10)
-#    motionProxy.closeHand(handName)
-#    handName  = 'RHand'
-#    motionProxy.openHand(handName)
-#    time.sleep(10)
-#    motionProxy.closeHand(handName)
+def userReadyToPlay(motionProxy, postureProxy):
+# =============================================================================
+# Have to record the transformation for ready to play position
+# =============================================================================
+    postureProxy.goToPosture("Crouch", 0.4)
+    time.sleep(1)
+    
+    chainName        = "RArm"
+    frame            = motion.FRAME_TORSO
 
-def playXylophone(motionProxy, keys):
+    transform       = []
+    fractionMaxSpeed = 0.5
+    axisMask         = 63 # this value include position and rotation
+
+    motionProxy.setTransforms(chainName, frame, transform, fractionMaxSpeed, axisMask)
+    
+    chainName        = "LArm"
+    frame            = motion.FRAME_TORSO
+
+    transform       = []
+    fractionMaxSpeed = 0.5
+    axisMask         = 63 # this value include position and rotation
+
+    motionProxy.setTransforms(chainName, frame, transform, fractionMaxSpeed, axisMask)
+    
+    motionProxy.setStiffnesses("LLeg", 0.2)
+    motionProxy.setStiffnesses("RLeg", 0.2)
+    time.sleep(1.0)
+    
+def playXylophone(motionProxy, keys, dt):
     # input notes is dictionary type, including key as note, and 
     # values as set of angles
     # anglse related to hit will be a seperate list
-#    motionProxy.setStiffnesses("Body", 0)
-    
-    
+   
     motionProxy.setStiffnesses("LArm", 1)
     motionProxy.setStiffnesses("RArm", 1)
     
@@ -128,12 +142,12 @@ def playXylophone(motionProxy, keys):
 #                          [beforeHit[0], onHit, afterHit[0]]]
             
 
-            timeList = [[0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3]]
+            timeList = [[dt],
+                        [dt],
+                        [dt],
+                        [dt],
+                        [dt],
+                        [dt]]
             
             motionProxy.angleInterpolationBezier(['RArm'], timeList, angleList)
             
@@ -168,12 +182,12 @@ def playXylophone(motionProxy, keys):
 #                          [beforeHit[0], onHit, afterHit[0]]]
             
 
-            timeList = [[0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3],
-                        [0.3]]
+            timeList = [[dt],
+                        [dt],
+                        [dt],
+                        [dt],
+                        [dt],
+                        [dt]]
             
             motionProxy.angleInterpolationBezier(['LArm'], timeList, angleList)
             
@@ -187,7 +201,6 @@ def playXylophone(motionProxy, keys):
         
             motionProxy.angleInterpolationBezier(['LWristYaw'], timeLists, angleLists)
             
-#            time.sleep(0.1)           
 # =============================================================================
 # 
 # =============================================================================
@@ -213,107 +226,223 @@ def main(robotIP, PORT=9559):
 # =============================================================================
 #   Start play Harry Potter Theme as demo
 #   NAO plays Harry Potter Theme
-    keys = [3,6,8,7,6,10,9,7,0,6,8,7,5,7,3,1,3,
-            3,6,8,7,6,10,9,7,0,6,8,7,5,7,3,1,3]
-    # need better starting point for both arms, record the transform Friday
+    keys = [3,0,6,0,0,8,7,0,6,0,0,10,0,9,0,0,7,0,0,6,0,8,7,0,5,0,0,7,0,3,0,1,3,
+            3,0,6,0,0,8,7,0,6,0,0,10,0,9,0,0,7,0,0,6,0,8,7,0,5,0,0,7,0,3,0,1,3]
+#   need better starting point for both arms, record the transform Friday
     userInitPosture(motionProxy, postureProxy)
-    playXylophone(motionProxy, keys)
+    userReadyToPlay(motionProxy, postureProxy)
+    playXylophone(motionProxy, keys, dt = 0.3)
+    userReadyToPlay(motionProxy, postureProxy)
+    userInitPosture(motionProxy, postureProxy)
     ledProxy.randomEyes(2.0)
     tts.say("Do you recognize this song from somewhere?")
     time.sleep(3.0)
     tts.say("Yes, it is the the most popular theme from Movie Harry Potter!")
     time.sleep(1.0)
-    # may use speech recognition instead of this
+#   may use speech recognition instead of this
     tts.say("Do you like it?")
     time.sleep(3.0)
-    tts.say("Great! Let me tell you hot to play this song")
+    tts.say("Great! Let me tell you how to play this song")
     time.sleep(2.0)    
     
 # =============================================================================
-# =============================================================================
-#   Start single note play without color
-    repeat = raw_input("User selection 1 or 0: ");
-    while repeat:
-        key1 = [6]
-        name = 'FaceLeds'
-        colorName1 = 'blue'
-        duration = 1.0
+#   creating for loop to control the task including repeat task and take break
+    for i in range(1000):
         
-        ledProxy.fadeRGB(name, colorName1, duration)
-        playXylophone(motionProxy, key1)
-        userInitPosture(motionProxy,postureProxy)
-        tts.say("I just played a note, can you repeat that note for me?")
-        time.sleep(1.0)
-        tts.say("You may find a pair of red head mallet on the table somewhere.")
-        time.sleep(1.0)
-        tts.say("I want you to pick them up, and use one of them to play that note.")
-        time.sleep(5.0)
-        # may have to create a if condition depending on user's response
-        tts.say("Do you want to try this again?")
-        time.sleep(4.0)
-        repeat = int(raw_input("User selection 1 or 0: "))
+        taskNumber = int(raw_input("select task:\n\
+                                   1: single note play\n\
+                                   2: single note with color\n\
+                                   3: multiple notes with color\n\
+                                   4: first half song practice\n\
+                                   5: second half song practice\n\
+                                   6: whole song play\n\
+                                   7: take break\n\
+                                   8: free play\n\
+                                   0: end session\n\
+                                   please make selection: "))
         
 # =============================================================================
-# Start single note play along with color  
+#       task 1: Start single note play without color
+        if taskNumber == 1:
+            
+            keys = [6]
+            name = 'FaceLeds'
+            colorName1 = 'blue'
+            duration = 1.0
+        
+            ledProxy.fadeRGB(name, colorName1, duration)
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 0.3)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("I just played a note, can you repeat that note for me?")
+            time.sleep(1.0)
+            tts.say("You may find a pair of red head mallet \
+                    on the table somewhere.")
+            time.sleep(1.0)
+            tts.say("I want you to pick them up, \
+                    and use one of them to play that note.")
+            time.sleep(5.0)
+            # may have to create a if condition depending on user's response
+            tts.say("Do you want to try this again?")
+        
+# =============================================================================
+#       task 2: Start single note play along with color  
+        elif taskNumber == 2: 
+            
+            keys = [8]
+            name = 'FaceLeds'
+            colorName2 = 'green'
+            duration = 1.0
+            
+            tts.say("Wonderful! Here comes an other one!")
+            ledProxy.fadeRGB(name, colorName2, duration)
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 0.3)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)    
+            tts.say("This is a new note, can you repeat that note for me?")
+            time.sleep(5.0)
+            tts.say("Have you notice that my eye color matchs the note color?")
+            time.sleep(2.0)
+            tts.say("Let's try it again, I am going to hit the green bar now, \
+                    listen carefully!")
+            ledProxy.fadeRGB(name, colorName2, duration)
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 0.3)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("Now, it is your turn to play the green bar!")
+            tts.say("And try to use your left hand to do this.")
+            time.sleep(5.0)
+            tts.say("Do you want to try this again?")
+        
+# =============================================================================
+#       task 3: Start multiple notes play along with color
+        elif taskNumber == 3:
+                
+            keys = [3, 6, 8]
+            name = 'FaceLeds'
+#            colorNames = ['red', 'green', 'blue']
+            duration = 0.5
+            
+            tts.say("In the next part we are going to have more fun!")
+            ledProxy.randomEyes(2.0)
+        
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 0.5)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("I just played three notes, can you repeat them for me?")
+            time.sleep(5.0)
+            tts.say("Great Job!")
+            time.sleep(1.0)
+            tts.say("Now, if you can sing the color while hitting the note \
+                    that would be even better!")
+            time.sleep(5.0)
+            tts.say("Well done!")
+            tts.say("Do you want to try it again?")
+        
+# =============================================================================
+#       task 4: Start play whole song 
+#       first half song
+        elif taskNumber == 4:
+            
+            keys = [3,6,8,7,6,10,9,7]
+            name = 'FacdLeds'
+#            colorRGB = ['0x00FF0000', '0x0000FF00', '0x000000FF',
+#                        '0x00FF00FF', '0x00C0C0C0', '0x00A16400']
+            duration = 0.5
+            
+            tts.say("Here comes the ultimate challenge!")
+            tts.say("We are going to play the Harry Potter Song \
+                    for the rest of the session.")
+            time.sleep(1.0)
+            tts.say("Here comes the first half of the song! \
+                    Listen and watch carefully.")
+            tts.say("I will use slower speed to play.")
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 1)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("Did you get it?")
+            time.sleep(2.0)
+            tts.say("Now, it is your turn to play.")
+            time.sleep(20.0)
+            tts.say("Do you want to try it again?")
+        
+# =============================================================================
+#       task 5: Start play whole song 
+#       second half song 
+        elif taskNumber == 5:
+            
+            keys = [6,8,7,5,7,3,1,3]
+            name = 'FacdLeds'
+#            colorRGB = ['0x00FF0000', '0x0000FF00', '0x000000FF',
+#                        '0x00FF00FF', '0x00C0C0C0', '0x00A16400']
+            duration = 0.5
 
-    key2 = [8]
-    name = 'FaceLeds'
-    colorName2 = 'green'
-    duration = 1.0
-    
-    tts.say("Wonderful! Here comes an other one!")
-    ledProxy.fadeRGB(name, colorName2, duration)
-    playXylophone(motionProxy, key2)
-    userInitPosture(motionProxy, postureProxy)
-    tts.say("This is a new note, can you repeat that note for me?")
-    time.sleep(5.0)
-    tts.say("Have you notice that my eye color matchs the note color?")
-    time.sleep(2.0)
-    tts.say("Let's try it again, I am going to hit the green bar now, listen carefully!")
-    repeat = raw_input("User selection 1 or 0: ")
-    while repeat:
-        ledProxy.fadeRGB(name, colorName2, duration)
-        playXylophone(motionProxy, key2)
-        userInitPosture(motionProxy, postureProxy)
-        tts.say("Now, it is your turn to play the green bar!")
-        tts.say("And try to use your left hand to do this.")
-        time.sleep(5.0)
-        tts.say("Do you want to try this again?")
-        time.sleep(4.0)
-        repeat = int(raw_input("User selection 1 or 0: "))
-    
-# =============================================================================
-# Start multiple notes play along with color
-    key3 = [3, 6, 8]
-    name = 'FaceLeds'
-    colorNames = ['red', 'green', 'blue']
-    duration = [0.5, 1.0, 0.5]
-    
-    tts.say("Now we are goint to play something more fun!")
-    ledProxy.randomEyes(2.0)
-    repeat = raw_input("User selection 1 or 0: ")
-    while repeat:
-        playXylophone(motionProxy, key3)
-        userInitPosture(motionProxy, postureProxy)
-        tts.say("I just played three notes, can you repeat them for me?")
-        time.sleep(5.0)
-        tts.say("Great Job!")
-        time.sleep(1.0)
-        tts.say("Now, if you can sing the color and hit the note that would be even better!")
-        time.sleep(5.0)
-        tts.say("Well done!")
-        tts.say("Do you want to try it again?")
-        time.sleep(4.0)
-        repeat = int(raw_input("User selection 1 or 0: "))
-        
-    
-    
-    
+            tts.say("Here comes the second half of the song! \
+                    Listen and watch carefully.")
+            tts.say("I will use slower speed to play.")
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 1)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("Did you get it?")
+            time.sleep(2.0)
+            tts.say("Now, it is your turn to play.")
+            time.sleep(20.0)
+            tts.say("Do you want to try it again?")
 
-    
-    
-    
 # =============================================================================
+#       task 6: play the whole song
+        elif taskNumber == 6:
+            keys = [3,0,6,0,0,8,7,0,6,0,0,10,0,9,0,0,7,0,0,
+                    6,0,8,7,0,5,0,0,7,0,3,0,1,3]
+            name = 'FacdLeds'
+#            colorRGB = ['0x00FF0000', '0x0000FF00', '0x000000FF',
+#                        '0x00FF00FF', '0x00C0C0C0', '0x00A16400']
+            duration = 0.5
+
+            tts.say("Here comes the whole song! \
+                    Listen and watch carefully.")
+            tts.say("I will use normal speed to play.")
+            userInitPosture(motionProxy, postureProxy)
+            userReadyToPlay(motionProxy, postureProxy)
+            playXylophone(motionProxy, keys, dt = 0.3)
+            userReadyToPlay(motionProxy, postureProxy)
+            userInitPosture(motionProxy,postureProxy)
+            tts.say("Did you get it?")
+            time.sleep(2.0)
+            tts.say("Now, it is your turn to play.")
+            time.sleep(20.0)
+            tts.say("Do you want to try it again?")            
+
+# =============================================================================
+#       task 7: take a break for 180 seconds      
+        elif taskNumber == 7:
+            
+            tts.say("Do you want to take a break?")
+            tts.say("You will have one hundred and eighty seconds \
+                    for a short break!")
+            tts.say("Starting now!")
+            ledProxy.randomEyes(5.0)
+            time.sleep(180)
+            tts.say("Shall we start the next task?")  
+        
+        
+# =============================================================================
+#       task 9: end the session get out the loop
+        elif taskNumber == 9:
+            break
+        
 # =============================================================================
 # Calling the main
 if __name__ == "__main__":

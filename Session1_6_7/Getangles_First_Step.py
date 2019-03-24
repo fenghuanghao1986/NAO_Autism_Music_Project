ab1a885ed901c3718df19b16235eb21a653b572f
FingerStyleFinn <hfeng7@du.edu>
2019-03-23 21:23:20 -0600

    Update Getangles_First_Step.py
    get new angles

--- Session1_6_7/Getangles_First_Step.py
+++ Session1_6_7/Getangles_First_Step.py
@@ -11,14 +11,14 @@ def main(robotIP, PORT=9559):
     postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
     tts = ALProxy("ALTextToSpeech", robotIP, PORT)
     
-    postureProxy.goToPosture("Crouch", 0.4)
+    #postureProxy.goToPosture("Crouch", 0.4)
     time.sleep(1)
     
     motionProxy.setStiffnesses("Head", 0.2)
     motionProxy.setStiffnesses("LLeg", 1)
     motionProxy.setStiffnesses("RLeg", 1)
-#    motionProxy.setAngles("LHand", 0.22, 1)
-    motionProxy.setAngles("RHand", 0.22, 1)
+    motionProxy.setAngles("LHand", 0.22, 1)
+#    motionProxy.setAngles("RHand", 0.22, 1)
 
     
     # getting both legs angles for later set legs angles
@@ -61,37 +61,37 @@ def main(robotIP, PORT=9559):
     
     tts.say("Move arm to get desired angles!")
     
-#    motionProxy.setStiffnesses("LArm", 0.0)
-#    motionProxy.setStiffnesses("RArm", 0.0)
+    motionProxy.setStiffnesses("LArm", 0.0)
+    motionProxy.setStiffnesses("RArm", 0.0)
        
     # Example that finds the difference between the command and sensed angles.
 
     time.sleep(10.0)
     
-    motionProxy.setStiffnesses("LArm", 1.0)
-#    motionProxy.setStiffnesses("RArm", 1.0)
-    motionProxy.setAngles("LHand", 0.22, 1)
-#    motionProxy.setAngles("RHand", 0.22, 1)
+#    motionProxy.setStiffnesses("LArm", 1.0)
+    motionProxy.setStiffnesses("RArm", 1.0)
+#    motionProxy.setAngles("LHand", 0.22, 1)
+    motionProxy.setAngles("RHand", 0.22, 1)
 
-#    names         = "RArm"
-    names = "LArm"
+    names         = "RArm"
+#    names = "LArm"
     useSensors  = True
     frame  = motion.FRAME_TORSO
-#    sensorAngles = motionProxy.getAngles(names, useSensors)
-#    print "Sensor angles:"
-#    print str(sensorAngles)
-#    print ""
+    sensorAngles = motionProxy.getAngles(names, useSensors)
+    print "Sensor angles:"
+    print str(sensorAngles)
+    print ""
 
-    result = motionProxy.getTransform(names, frame, useSensors)
-    print(result)
+#    result = motionProxy.getTransform(names, frame, useSensors)
+#    print(result)
     tts.say("Angle recorded!")
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-#    parser.add_argument("--ip", type=str, default="192.168.0.2",
-#                        help="Robot ip address")
-    parser.add_argument("--ip", type=str, default="127.0.0.1",
+    parser.add_argument("--ip", type=str, default="192.168.0.2",
                         help="Robot ip address")
+#    parser.add_argument("--ip", type=str, default="127.0.0.1",
+#                        help="Robot ip address")
     parser.add_argument("--port", type=int, default=9559,
                         help="Robot port number")
 

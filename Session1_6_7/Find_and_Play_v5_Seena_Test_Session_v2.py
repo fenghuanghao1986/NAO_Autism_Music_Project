9843fcaa909cdff1559efa4477bc4bd103533e91
FingerStyleFinn <hfeng7@du.edu>
2019-04-06 10:24:16 -0600

    Update Find_and_Play_v5_Seena_Test_Session_v2.py
    modify challenges

--- Session1_6_7/Find_and_Play_v5_Seena_Test_Session_v2.py
+++ Session1_6_7/Find_and_Play_v5_Seena_Test_Session_v2.py
@@ -370,19 +370,20 @@ def main(robotIP, PORT=9559):
                                    8: free play\n\
                                    9: end session\n\
                                    10: ask robot show again\n\
-                                   11: robot ask kid try it again\n\
+                                   11: well done move on next one\n\
+                                   12: robot ask kid try it again\n\
                                    please make selection: "))
         
 # =============================================================================
         if taskNumber == 0:
 #           Intro to entire session
-            tts.say("Hello, my friend!")
+            tts.say("Hello, Seena!")
             time.sleep(0.5)
             tts.say("Welcome back to NAO music party!")
             time.sleep(1.0)
-            tts.say("Today, we are going to play a lovely song!")
+            tts.say("Today, we are going to learn a lovely song!")
             ledProxy.randomEyes(2.0)
-            tts.say("Let me show you what I have learned lately!")
+            tts.say("Let me show you how to play!")
             time.sleep(0.5)   
 # =============================================================================
 #           Play twinkle twinkle
@@ -399,27 +400,33 @@ def main(robotIP, PORT=9559):
             userInitPosture(motionProxy, postureProxy)
             ledProxy.randomEyes(2.0)
             tts.say("Do you recognize this song from somewhere?")
-            time.sleep(3.0)
+            time.sleep(5.0)
             tts.say("Yes, it is the the most popular Twinkle Twinkle Little Star!")
-            time.sleep(1.0)
+            time.sleep(3.0)
 #           may use speech recognition instead of this
             tts.say("Do you like it?")
-            time.sleep(3.0)
-            tts.say("Do you want to hear another one?")
-            if int(raw_input("1 for play new song, 2 for no:")):
-                
-                tts.say("Great! Let me tell you how to play this song")
+            time.sleep(5.0)
+            tts.say("Let's start our practice!")
+            tts.say("You may find a pair of red head mallet \
+                    on the table somewhere.")
+            time.sleep(1.0)
+            tts.say("I want you to pick them up, \
+                    and get ready to play following my instruction.")
+#            if int(raw_input("1 for play new song, 2 for no:")):
+#                
+#                tts.say("Great! Let me tell you how to play this song")
             
 # =============================================================================
 # =============================================================================
 #       task 1: Start single note play without color
         elif taskNumber == 1:
             
-            keys = [6]
+            keys = [1]
             name = 'FaceLeds'
-            colorName1 = 'blue'
+            colorName1 = 'green'
             duration = 1.0
-        
+            tts.say("Welcome to the strike challenge!")
+
             ledProxy.fadeRGB(name, colorName1, duration)
             userInitPosture(motionProxy, postureProxy)
             userReadyToPlay(motionProxy, postureProxy)
@@ -429,23 +436,18 @@ def main(robotIP, PORT=9559):
             userInitPosture(motionProxy,postureProxy)
             tts.say("I just played a note, can you repeat that note for me?")
             time.sleep(1.0)
-            tts.say("You may find a pair of red head mallet \
-                    on the table somewhere.")
-            time.sleep(1.0)
-            tts.say("I want you to pick them up, \
-                    and use one of them to play that note.")
 
         
 # =============================================================================
 #       task 2: Start single note play along with color  
         elif taskNumber == 2: 
             
-            keys = [8]
+            keys = [6]
             name = 'FaceLeds'
-            colorName2 = 'green'
+            colorName2 = 'blue'
             duration = 1.0
             
-            tts.say("Wonderful! Here comes an other one!")
+            tts.say("Welcome to the color challenge!")
             ledProxy.fadeRGB(name, colorName2, duration)
             userInitPosture(motionProxy, postureProxy)
             userReadyToPlay(motionProxy, postureProxy)
@@ -456,8 +458,8 @@ def main(robotIP, PORT=9559):
             tts.say("This is a new note, can you repeat that note for me?")
             time.sleep(5.0)
             tts.say("Have you notice that my eye color matchs the note color?")
-            time.sleep(2.0)
-            tts.say("Let's try it again, I am going to hit the green bar now, \
+            time.sleep(5.0)
+            tts.say("Let's try it again, I am going to hit the blue bar now, \
                     listen carefully!")
             ledProxy.fadeRGB(name, colorName2, duration)
             userInitPosture(motionProxy, postureProxy)
@@ -480,7 +482,7 @@ def main(robotIP, PORT=9559):
 #            colorNames = ['red', 'green', 'blue']
             duration = 0.5
             
-            tts.say("In the next part we are going to have more fun!")
+            tts.say("Welcome to the triple blend challenge!")
             ledProxy.randomEyes(2.0)
         
             userInitPosture(motionProxy, postureProxy)
@@ -489,15 +491,13 @@ def main(robotIP, PORT=9559):
             playXylo(motionProxy, keys, dt1)
             userReadyToPlay(motionProxy, postureProxy)
             userInitPosture(motionProxy,postureProxy)
-            tts.say("I just played three notes, can you repeat them for me?")
+            tts.say("I just played three notes, can you repeat them for me? \
+                    make sure you followed by the proper color order\
+                    for example green, gray, blue.")
             time.sleep(5.0)
-            tts.say("Great Job!")
-            time.sleep(1.0)
-            tts.say("Now, if you can sing the color while hitting the note \
-                    that would be even better!")
-            time.sleep(5.0)
-            tts.say("Well done!")
-            tts.say("Do you want to try it again?")
+#            tts.say("Now, if you can sing the color while hitting the note \
+#                    that would be even better!")
+
         
 # =============================================================================
 #       task 4: Start play whole song 
@@ -511,10 +511,7 @@ def main(robotIP, PORT=9559):
             duration = 0.5
             
             tts.say("Here comes the ultimate challenge!")
-            tts.say("We are going to play the Twinkle Twinkle Song \
-                    for the rest of the session.")
-            time.sleep(1.0)
-            tts.say("Here comes the first half of the song! \
+            tts.say("This is the first half of the song! \
                     Listen and watch carefully.")
             tts.say("I will use slower speed to play.")
             userInitPosture(motionProxy, postureProxy)
@@ -523,22 +520,24 @@ def main(robotIP, PORT=9559):
             playXylo(motionProxy, keys, dt1)
             userReadyToPlay(motionProxy, postureProxy)
             userInitPosture(motionProxy,postureProxy)
-            tts.say("Did you get it?")
-            time.sleep(1.0)
+#            tts.say("Did you get it?")
+#            time.sleep(1.0)
             tts.say("Now, it is your turn to play.")
+            tts.say("If you need help, please say Help.")
 
 # =============================================================================
 #       task 5: Start play whole song 
 #       second half song 
         elif taskNumber == 5:
             
-            keys = [5,5,4,4,3,3,2,0,5,5,4,4,3,3,2,0]
+            keys = [0,0,5,5,4,4,3,3,2,0,5,5,4,4,3,3,2,0]
             name = 'FacdLeds'
 #            colorRGB = ['0x00FF0000', '0x0000FF00', '0x000000FF',
 #                        '0x00FF00FF', '0x00C0C0C0', '0x00A16400']
             duration = 0.5
 
-            tts.say("Here comes the second half of the song! \
+            tts.say("Here comes the ultimate challenge! \
+                    This is second half of the song\
                     Listen and watch carefully.")
             tts.say("I will use slower speed to play.")
             userInitPosture(motionProxy, postureProxy)
@@ -547,9 +546,11 @@ def main(robotIP, PORT=9559):
             playXylo(motionProxy, keys, dt1)
             userReadyToPlay(motionProxy, postureProxy)
             userInitPosture(motionProxy,postureProxy)
-            tts.say("Did you get it?")
-            time.sleep(1.0)
+#            tts.say("Did you get it?")
+#            time.sleep(1.0)
             tts.say("Now, it is your turn to play.")
+            tts.say("If you need help, please say Help.")
+
 
 # =============================================================================
 #       task 6: play the whole song
@@ -562,7 +563,8 @@ def main(robotIP, PORT=9559):
 #                        '0x00FF00FF', '0x00C0C0C0', '0x00A16400']
             duration = 0.5
 
-            tts.say("Here comes the whole song! \
+            tts.say("Final challenge! \
+                    Here comes the whole song! \
                     Listen and watch carefully.")
             tts.say("I will use normal speed to play.")
             userInitPosture(motionProxy, postureProxy)
@@ -571,27 +573,29 @@ def main(robotIP, PORT=9559):
             playXylo(motionProxy, keys, dt)
             userReadyToPlay(motionProxy, postureProxy)
             userInitPosture(motionProxy,postureProxy)
-            tts.say("Did you get it?")
-            time.sleep(1.0)
-            tts.say("Now, it is your turn to play.")      
+#            tts.say("Did you get it?")
+#            time.sleep(1.0)
+            tts.say("Now, it is your turn to play.")
+            tts.say("If you need help, please say Help.")
+
 
 # =============================================================================
 #       task 7: take a break for 180 seconds      
         elif taskNumber == 7:
             
             tts.say("Do you want to take a break?")
-            tts.say("You will have 180 seconds \
+            tts.say(" If yes, You will have 180 seconds \
                     for a short break!")
-            tts.say("Starting now!")
-            ledProxy.randomEyes(3.0)
-            time.sleep(57)
-            tts.say("You have 120 seconds left.")
-            ledProxy.randomEyes(3.0)
-            time.sleep(57)
-            tts.say("You have one minunt left.")
-            ledProxy.randomEyes(3.0)
-            time.sleep(57)
-            tts.say("Shall we start the next task?")  
+#            tts.say("Starting now!")
+#            ledProxy.randomEyes(3.0)
+#            time.sleep(57)
+#            tts.say("You have 120 seconds left.")
+#            ledProxy.randomEyes(3.0)
+#            time.sleep(57)
+#            tts.say("You have one minunt left.")
+#            ledProxy.randomEyes(3.0)
+#            time.sleep(57)
+#            tts.say("Shall we start the next task?")  
         
 # =============================================================================
 #       task 8: free play
@@ -601,6 +605,7 @@ def main(robotIP, PORT=9559):
             time.sleep(1.0)
             tts.say("Feel free to stay and play whatever song you may like.")
             time.sleep(1.0)
+            tts.say("If you decide to leave, please say Goodbye to me.")
 
 # =============================================================================
 #       task 9: end the session get out the loop
@@ -612,13 +617,20 @@ def main(robotIP, PORT=9559):
 #       other typo or mistakes
             
         elif taskNumber == 10:
-            tts.say("Do you want me to show you again?")
+            tts.say("It looks like you may need some help. \
+                    Do you want me to show you again?")
             motionProxy.rest()
             
         elif taskNumber == 11:
-            tts.say("It looks like you may need want to try it again.")
+            tts.say("Well done! You just unlocked a new challenge, \
+                    let's try it now!")
+            motionProxy.rest()
             
-        elif taskNumber == 
+        elif taskNumber == 12:
+            tts.say("It doesn't sound 100 percentage right, \
+                    please try it again. I am listening.")
+            motionProxy.rest()
+            
         else:
             continue
         

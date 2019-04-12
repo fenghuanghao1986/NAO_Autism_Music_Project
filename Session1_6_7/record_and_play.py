0a18d05afcf2a35491b3ebf8be53e456d87c616d
FingerStyleFinn <hfeng7@du.edu>
2019-04-12 14:00:19 -0600

    Update record_and_play.py

--- Session1_6_7/record_and_play.py
+++ Session1_6_7/record_and_play.py
@@ -9,31 +9,31 @@ Created on Thu Apr 11 11:21:29 2019
 #include <alproxies/alaudiorecorderproxy.h>
 #include <qi/os.hpp>
 
-int main(int argc, char **argv)
-{
-  if (argc < 2) {
-    std::cerr << "Usage: alaudiorecorder_startrecording pIp"
-              << std::endl;
-    return 1;
-  }
-  const std::string pIp = argv[1];
+#int main(int argc, char **argv)
+#{
+#  if (argc < 2) {
+#    std::cerr << "Usage: alaudiorecorder_startrecording pIp"
+#              << std::endl;
+#    return 1;
+#  }
+#  const std::string pIp = argv[1];
+  channels = ALProxy("ALAudioRecorderProxy", IP, PORT)
+#  AL::ALAudioRecorderProxy proxy(pIp);
 
-  AL::ALAudioRecorderProxy proxy(pIp);
-
-  /// Configures the channels that need to be recorded.
+#  /// Configures the channels that need to be recorded.
   AL::ALValue channels;
-  channels.arrayPush(0); //Left
-  channels.arrayPush(0); //Right
-  channels.arrayPush(1); //Front
-  channels.arrayPush(0); //Rear
+  channels.arrayPush(0); #//Left
+  channels.arrayPush(0); #//Right
+  channels.arrayPush(1); #//Front
+  channels.arrayPush(0); #//Rear
 
-  /// Starts the recording of NAO's front microphone at 16000Hz
-  /// in the specified wav file
+#  /// Starts the recording of NAO's front microphone at 16000Hz
+#  /// in the specified wav file
   proxy.startMicrophonesRecording("/home/nao/test.wav", "wav", 16000, channels);
 
   qi::os::sleep(5);
 
-  /// Stops the recording and close the file after 10 seconds.
+#  /// Stops the recording and close the file after 10 seconds.
   proxy.stopMicrophonesRecording();
 
   return 0;

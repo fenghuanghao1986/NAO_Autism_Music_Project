6ae74b911cb6be83daa76fb3015c464ccdf85d1c
FingerStyleFinn <hfeng7@du.edu>
2019-04-14 10:56:27 -0600

    Update record_and_play.py
    test it on robot and debug later on.

--- Session1_6_7/record_and_play.py
+++ Session1_6_7/record_and_play.py
@@ -9,6 +9,11 @@ Created on Thu Apr 11 11:21:29 2019
 #include <alproxies/alaudiorecorderproxy.h>
 #include <qi/os.hpp>
 
+import sys
+import time
+import argparse
+from naoqi import ALProxy
+
 #int main(int argc, char **argv)
 #{
 #  if (argc < 2) {
@@ -17,55 +22,60 @@ Created on Thu Apr 11 11:21:29 2019
 #    return 1;
 #  }
 #  const std::string pIp = argv[1];
-  channels = ALProxy("ALAudioRecorderProxy", IP, PORT)
-#  AL::ALAudioRecorderProxy proxy(pIp);
 
-#  /// Configures the channels that need to be recorded.
-  AL::ALValue channels;
-  channels.arrayPush(0); #//Left
-  channels.arrayPush(0); #//Right
-  channels.arrayPush(1); #//Front
-  channels.arrayPush(0); #//Rear
+def main(robotIP, PORT):
+    recordProxy = ALProxy("ALAudioRecorderProxy", robotIP, PORT)
+    #  AL::ALAudioRecorderProxy proxy(pIp);
+    
+    #  /// Configures the channels that need to be recorded.
+    channelsProxy = ALProxy("ALValue", robotIP, PORT)
+    #  AL::ALValue channels;
+    channelsProxy.arrayPush(0); #//Left
+    channelsProxy.arrayPush(0); #//Right
+    channelsProxy.arrayPush(1); #//Front
+    channelsProxy.arrayPush(0); #//Rear
+    
+    #  /// Starts the recording of NAO's front microphone at 24000Hz
+    #  /// in the specified wav file
+    recordProxy.startMicrophonesRecording("/home/nao/test.wav", "wav", 24000, channelsProxy);
+    
+    time.sleep(10);
+    
+    #  /// Stops the recording and close the file after 10 seconds.
+    recordProxy.stopMicrophonesRecording(); 
 
-#  /// Starts the recording of NAO's front microphone at 16000Hz
-#  /// in the specified wav file
-  proxy.startMicrophonesRecording("/home/nao/test.wav", "wav", 16000, channels);
+#    if (len(sys.argv) < 2):
+#        print "Usage: 'python audioplayer_play.py IP [PORT]'"
+#        sys.exit(1)
+#    
+#    IP = sys.argv[1]
+#    PORT = 9559
+    
+    if (len(sys.argv) > 2):
+        PORT = sys.argv[2]
+    try:
+        aup = ALProxy("ALAudioPlayer", robotIP, PORT)
+    except Exception,e:
+        print "Could not create proxy to ALAudioPlayer"
+        print "Error was: ",e
+        sys.exit(1)
+    
+    #plays a file and get the current position 5 seconds later
+    fileId = aup.post.playFile("/home/nao/test.wav")
+    
+    time.sleep(5)
+    
+    #currentPos should be near 5 secs
+    currentPos = aup.getCurrentPosition(fileId)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="192.168.0.2",
+                        help="Robot ip address")
+#    parser.add_argument("--ip", type=str, default="127.0.0.1",
+#                        help="Robot ip address")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Robot port number")
 
-  qi::os::sleep(5);
-
-#  /// Stops the recording and close the file after 10 seconds.
-  proxy.stopMicrophonesRecording();
-
-  return 0;
-}
-
-
-
-import sys
-import time
-from naoqi import ALProxy
-
-if (len(sys.argv) < 2):
-    print "Usage: 'python audioplayer_play.py IP [PORT]'"
-    sys.exit(1)
-
-IP = sys.argv[1]
-PORT = 9559
-
-if (len(sys.argv) > 2):
-    PORT = sys.argv[2]
-try:
-    aup = ALProxy("ALAudioPlayer", IP, PORT)
-except Exception,e:
-    print "Could not create proxy to ALAudioPlayer"
-    print "Error was: ",e
-    sys.exit(1)
-
-#plays a file and get the current position 5 seconds later
-fileId = aup.post.playFile("/usr/share/naoqi/wav/random.wav")
-
-time.sleep(5)
-
-#currentPos should be near 5 secs
-currentPos = aup.getCurrentPosition(fileId)
-
+    args = parser.parse_args()
+    main(args.ip, args.port)    

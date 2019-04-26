9b437fdce6f6b5b2da64e1a893acfa3a5b2a56b5
FingerStyleFinn <hfeng7@du.edu>
2019-04-25 20:00:15 -0600

    save data test
    this will be added to the session code
    also have to figure out how to log data in the middle of the session based on the input and result.

---
+++ Session1_6_7/save_data_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 13:55:59 2019
+
+@author: fengh
+"""
+
+# read and write data test function
+
+import numpy as np
+import csv
+import datetime
+
+print "Enter subject number:\n"
+subject = raw_input()
+print "Enter session number:\n"
+session = raw_input()
+now = datetime.datetime.now()
+day = str(now.day)
+mon = str(now.month)
+year = str(now.year)
+
+fileName = subject + '_' + session  + '_' + year + '_' + mon + '_' + day
+
+with open(fileName + '.csv', 'wb') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(['task', 'ground_truth', 'kid_input', 'result'])
+    filewriter.writerow(['01', '123', '122', '.667'])
+    filewriter.writerow(['01', '123', '122', '.667'])
+    filewriter.writerow(['01', '123', '122', '.667'])
+

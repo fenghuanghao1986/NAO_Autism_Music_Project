305bca570ad70005d511884a7171ed0d35996b07
FingerStyleFinn <hfeng7@du.edu>
2019-04-30 22:47:22 -0600

    have to create the function which can combine different notes as one file.

---
+++ Session1_6_7/notes_source/audio_generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 12:06:47 2019
+
+@author: fengh
+"""
+
+# This is the create music game test function
+# contains easy and hard modes
+# game type:
+# 1. music feeling: describe the feeling between different combo of notes
+# 2. ear play 1: listen two piece of notes with slicely difference or same 
+# 3. ear play 2: listen one piece and play it on xylophone robot provide feedback
+
+
+uncomfortable = [3,4,7,8,10,11]
+comfortable = [1,2,4,5,6,8,9]

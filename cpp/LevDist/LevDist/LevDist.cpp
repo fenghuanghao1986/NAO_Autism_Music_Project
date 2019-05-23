6a64bbfd475d3bfa66d3244b35d7788d00017028
FingerStyleFinn <hfeng7@du.edu>
2019-05-23 13:34:13 -0600

    levDist again
    this time use cpp
    but without hit, I have to think and create it myself

---
+++ cpp/LevDist/LevDist/LevDist.cpp
@@ -0,0 +1,16 @@
+// LevDist.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string a, b;
+
+}
+
+float levDist(string a, stirng b)
+{
+
+}

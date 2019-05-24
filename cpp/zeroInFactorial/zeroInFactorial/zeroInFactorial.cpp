1e33931c59c726ffb04cd14431a2136987d1b71c
FingerStyleFinn <hfeng7@du.edu>
2019-05-24 13:58:17 -0600

    new question
    related to zeros in N!

---
+++ cpp/zeroInFactorial/zeroInFactorial/zeroInFactorial.cpp
@@ -0,0 +1,11 @@
+// zeroInFactorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+     
+}

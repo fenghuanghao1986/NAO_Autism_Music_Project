d5eaa387b21f16fae6c8620ac39214079af48dda
FingerStyleFinn <hfeng7@du.edu>
2019-05-27 22:53:43 -0600

    Update Module_test_subscribe_test.py

--- July_Demo_Only/Module_test_subscribe_test.py
+++ July_Demo_Only/Module_test_subscribe_test.py
@@ -5,41 +5,57 @@ Created on Sun May 26 12:36:09 2019
 @author: CV_LAB_Howard
 """
 
-from naoqi import ALProxy, ALBroker, ALModule
+import sys
 import time
-check = 0
+
+from naoqi import ALProxy
+from naoqi import ALBroker
+from naoqi import ALModule
+
+from optparse import OptionParser
+
+NAO_IP = "nao.local"
 
 
-# create python module
-class myModule(ALModule):
-  """python class myModule test auto documentation : comment needed to create a new python module"""
+# Global variable to store the HumanGreeter module instance
+WordDetected = None
+memory = None
 
 
-  def pythondatachanged(self, strVarName, value):
-    """callback when data change"""
-    print "datachanged", strVarName, " ", value, " ", strMessage
-    global check
-    check = 1
+class WordDetectModule(ALModule):
+    """ A simple module able to react
+    to facedetection events
 
-  def _pythonPrivateMethod(self, param1, param2, param3):
-    global check
+    """
+    def __init__(self, name):
+        ALModule.__init__(self, name)
+        # No need for IP and port here because
+        # we have our Python broker connected to NAOqi broker
 
+        # Create a proxy to ALTextToSpeech for later use
+        self.tts = ALProxy("ALTextToSpeech")
 
-broker = ALBroker("pythonBroker","10.0.252.184",9999,"naoverdose.local",9559)
+        # Subscribe to the FaceDetected event:
+        global memory
+        memory = ALProxy("ALMemory")
+        memory.subscribeToEvent("WordRecognized",
+            "WordDetected",
+            "onWordDetected")
 
+    def onWordDetected(self, *_args):
+        """ This will be called each time a face is
+        detected.
 
-# call method
-try:
+        """
+        # Unsubscribe to the event when talking,
+        # to avoid repetitions
+        memory.unsubscribeToEvent("WordRecognized",
+            "WordDetected")
 
-  pythonModule = myModule("pythonModule")
-  prox = ALProxy("ALMemory")
-  #prox.insertData("val",1) # forbiden, data is optimized and doesn't manage callback
-  prox.subscribeToEvent("WordRecognized","pythonModule", "pythondatachanged") #  event is case sensitive !
+        self.tts.say("Hello, you")
 
-except Exception,e:
-  print "error"
-  print e
-  exit(1)
+        # Subscribe again to the event
+        memory.subscribeToEvent("WordRecognized",
+            "WordDetected",
+            "onWordDetected")
 
-while (1):
-  time.sleep(2)

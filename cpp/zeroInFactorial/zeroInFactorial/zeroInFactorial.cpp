01bce8bcdfdda62fe47ec64f7540392e432e1944
FingerStyleFinn <hfeng7@du.edu>
2019-05-27 22:53:40 -0600

    DONE!!!!

--- cpp/zeroInFactorial/zeroInFactorial/zeroInFactorial.cpp
+++ cpp/zeroInFactorial/zeroInFactorial/zeroInFactorial.cpp
@@ -5,7 +5,25 @@
 #include <math.h>
 using namespace std;
 
+double countZero(double n)
+{
+	double x, N = 0;
+	while (x >= 5)
+	{
+		N = n / 5;
+		x = n % 5;
+	}
+	return N;
+}
+
 int main()
 {
-     
+	double n, N;
+	cout << "Please input an interger: " << endl;
+	cin >> n;
+	N = countZero(n);
+	cout << "Result for " << n << " factorial contains "
+		<< N << " zeros at the end." << endl;
+	system("pause");
 }
+

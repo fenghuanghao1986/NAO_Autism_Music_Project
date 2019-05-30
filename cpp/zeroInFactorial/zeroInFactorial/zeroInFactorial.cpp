0baa81dbf35ec4825d4e1c2d46d759fd434a90d1
FingerStyleFinn <hfeng7@du.edu>
2019-05-30 00:12:14 -0600

    no message

--- cpp/zeroInFactorial/zeroInFactorial/zeroInFactorial.cpp
+++ cpp/zeroInFactorial/zeroInFactorial/zeroInFactorial.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 using namespace std;
 
-double countZero(int n)
+int countZero(int n)
 {
 	int x = 0, result = 0;
 	while (true)
@@ -19,14 +19,33 @@ double countZero(int n)
 	return result;
 }
 
+int countOne(int n)
+{
+	int x = 0, result = 0;
+	while (true)
+	{
+		x = n % 2;
+		n = n / 2;
+		result = result + n;
+		if (x != 0)
+			break;
+	}
+	result = result + 1;
+	return result;
+}
+
 int main()
 {
-	int n, N;
+	int n, N, M;
 	cout << "Please input an interger: " << endl;
 	cin >> n;
 	N = countZero(n);
+	M = countOne(n);
 	cout << "Result for " << n << " factorial contains "
 		<< N << " zeros at the end." << endl;
+
+	cout << "The position of the first one occurs in the result of " << n << " factorial is "
+		<< M << " ." << endl;
 	system("pause");
 }
 

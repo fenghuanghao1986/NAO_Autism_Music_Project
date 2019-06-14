a75dbb9218c4558809c8aefa329b5266c162b347
FingerStyleFinn <hfeng7@du.edu>
2019-06-14 16:25:07 -0600

    sub set problem

---
+++ cpp/maxSubset/maxSubset/maxSubset.cpp
@@ -0,0 +1,23 @@
+// maxSubset.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
+
+int insertSort(int arr[])
+{
+
+}
+
+int compare(int arr[])
+{
+	int posCnt = 0, negCnt = 0, zeroCnt = 0;
+	int n = arr[].length();
+	int newArr[n - 1];
+
+}

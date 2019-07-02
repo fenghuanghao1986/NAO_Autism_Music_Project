f633f388242d3a1e666e27aab6fbf9455453af99
FingerStyleFinn <hfeng7@du.edu>
2019-07-01 18:22:09 -0600

    Update test_00.py

--- July_Demo_Only/test_00.py
+++ July_Demo_Only/test_00.py
@@ -18,20 +18,20 @@ def main(robotIP, PORT = 9559):
     # Example showing multiple trajectories
     names      = ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 
                   'LElbowRoll', 'LWristYaw', 'LHand']
-    angleLists = [[-90.0*almath.TO_RAD, 0.0*almath.TO_RAD, 90.0*almath.TO_RAD], 
-                  [10.0*almath.TO_RAD, 76.0*almath.TO_RAD],
+    angleLists = [[-90.0*almath.TO_RAD], 
+                  [76.0*almath.TO_RAD],
                   [90.0*almath.TO_RAD, 0.0*almath.TO_RAD, -90.0*almath.TO_RAD],
                   [-2.0*almath.TO_RAD, -88.0*almath.TO_RAD, -2.0*almath.TO_RAD],
                   [90.0*almath.TO_RAD, 0.0*almath.TO_RAD, -90.0*almath.TO_RAD],
                   [1, 0, 1]]
-    timeLists  = [[0.5, 1.0, 1.5], 
-                  [2.0, 2.5],
+    timeLists  = [[0.5], 
+                  [2.0],
                   [3.0, 3.5, 4.0],
                   [4.5, 5.0, 5.5],
                   [6.0, 6.5, 7.0],
                   [7.5, 8.0, 8.5]]
     motionProxy.angleInterpolationBezier(names, timeLists, angleLists)
-    postureProxy.goToPosture("StandZero", 0.5)
+#    postureProxy.goToPosture("StandZero", 0.5)
 
     motionProxy.setStiffnesses("LArm", 0.0)
 
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="127.0.0.1",
                         help="Robot ip address")
-    parser.add_argument("--port", type=int, default=57008,
+    parser.add_argument("--port", type=int, default=51587,
                         help="Robot port number")
 
     args = parser.parse_args()

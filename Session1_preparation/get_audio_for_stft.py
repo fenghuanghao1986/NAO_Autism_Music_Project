bfb06d2bad0b92f9dd42cf26b7b2fcd66a9d3d3e
FingerStyleFinn <hfeng7@du.edu>
2019-07-04 14:12:25 -0600

    test audio download

--- Session1_preparation/get_audio_for_stft.py
+++ Session1_preparation/get_audio_for_stft.py
@@ -7,16 +7,27 @@ Created on Thu Jul  4 13:55:41 2019
 
 import recordplay
 import ssh
+import time
 
-host = "192.168.0.2"
+host = "192.168.0.3"
 username = "nao"
 pw = "nao"
-
+print("ready to record in 3 seconds")
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+print("start recording")
 recordplay.record(host, 9559, t=30)
+print("stop record!")
     
 origin = '/home/nao/uplay.wav'
 dst = r'C:\Users\fengh\Desktop\test_sampe_for_stft.wav'
- 
-ssh = SSHConnection(host, username, pw)
-ssh.get(origin, dst)
-ssh.close()
+
+sshFile = ssh.SSHConnection(host, username, pw)
+print("start downloading...")
+sshFile.get(origin, dst)
+print("download complete!")
+sshFile.close()

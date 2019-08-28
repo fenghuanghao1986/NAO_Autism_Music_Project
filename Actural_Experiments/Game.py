4af72c0bcd6cf58262572a0c47e4087f8bc12bbe
FingerStyleFinn <hfeng7@du.edu>
2019-08-28 11:07:02 -0600

    update for game song list

--- Actural_Experiments/Game.py
+++ Actural_Experiments/Game.py
@@ -83,7 +83,7 @@ songBank = {"Twinkle": [0,1,1,5,5,6,6,5,0,4,4,3,3,2,2,1,0,
                           7,7,7,0,7,7,7,0,7,9,5,6,7,0,
                           8,8,8,8,8,7,7,7,9,9,8,6,5]}
 
-song_names = ["Twinkle", "Promise", "Sunshine", "BabySharkv1","BabySharkv2", "BabySharkv3", "JingleBell"]
+song_names = ["Twinkle", "Promise", "Sunshine", "BabySharkv1", "JingleBell"]
 
 print "Enter subject name:\n"
 kid_name = raw_input()

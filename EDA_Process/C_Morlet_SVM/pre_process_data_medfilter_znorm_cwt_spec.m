6ee6e63ac3a8c737a1221092fae090975b807559
FingerStyleFinn <hfeng7@du.edu>
2019-12-27 18:57:59 -0700

    creating new data process m file

---
+++ EDA_Process/C_Morlet_SVM/pre_process_data_medfilter_znorm_cwt_spec.m
@@ -0,0 +1,35 @@
+clc;
+clear;
+warning off
+
+dataPath = 'D:\LabWork\ThesisProject\Music_Autism_Robot\EDA_Process\C_Morlet_SVM\EDA';
+fileType = '*.csv';
+
+dd = dir(fullFile(dataPath, fileType));
+fileNames = {dd.name};
+num = numel(fileNames);
+
+data = cell(num, 2);
+data(:,1) = regexprep(fileNames, '.csv', '');
+znormFilter = cell(num, 1);
+scaleRange = 1:100;
+
+for fileNum = 1: numel(fileNames)
+    
+    data{fileNum, 2} = dlmread(fullfile(dataPath, fileNames{fileNum}));
+    fprintf('Reading CSV sensor data...\n');
+    dataQ = data{fileNum, 2}(:, 6);
+    
+    [znormQ, muQ, sigmaQ] = zscore(dataQ);
+    fprintf('Znorm done for file %d... \n', file_num);
+    
+    znormFilter{fileNum} = medfilt1(znormQ.', 1);
+    
+    znormCWT = abs(cwt(znormFilter{fileNum}, scaleRange, 'cmor1.5-2'));
+    znormCWTSpect{fileNum, fileNum} = imresize(znormCWT, [100, 32], 'bicubic');
+    
+    BEpoch = 1: 10;
+    BaseMat = (znormCWTSpect{fileNum, fileNum}(:, BEpoch))';
+    BaseMean = repmat(mean(BaseMat)', 1, size(znormCWTSpect))
+    
+end
